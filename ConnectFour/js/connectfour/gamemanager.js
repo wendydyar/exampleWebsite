eaf6186b33066dcb79d517a79991a6c7b6e8babc
wendydyar <wendypino27@gmail.com>
2016-05-30 17:09:43 -0400

    Added hooks for GameModel a nd GameViewer. Provided additional check for null events.

--- ConnectFour/js/connectfour/gamemanager.js
+++ ConnectFour/js/connectfour/gamemanager.js
@@ -97,6 +97,14 @@ define(['connectfour/aifactory', 'connectfour/gamemodel', 'connectfour/gameviewe
 				_performPlayForAI();
 			}
         }
+
+        this.getGameModel = function(){
+			return _oGameModel;
+		}
+		
+		this.getGameViewer = function(){
+			return _oGameViewer;
+		}
 		
         this.handleStartNewGame = function(oOptions){
 			//Update the Model
@@ -126,10 +134,15 @@ define(['connectfour/aifactory', 'connectfour/gamemodel', 'connectfour/gameviewe
             }
             else {
                 //Create a new gamepiece
-				if(event.type == "mousedown"){
-					//If the gamepiece was created by a user's action, pass the event along to the gamepiece creation
-					_oGameViewer.setCurrentGamePiece(new gamepiece.GamePiece(_oGameModel.getCurrentPlayer().getId(), _oGameModel.getCurrentPlayer().getStyle(), _oGameModel.getCurrentPlayer().getWinStyle(), event, nColumnId));
-				}else{
+				if(event){
+					if(event.type == "mousedown"){
+					    //If the gamepiece was created by a user's action, pass the event along to the gamepiece creation
+					    _oGameViewer.setCurrentGamePiece(new gamepiece.GamePiece(_oGameModel.getCurrentPlayer().getId(), _oGameModel.getCurrentPlayer().getStyle(), _oGameModel.getCurrentPlayer().getWinStyle(), event, nColumnId));
+				    }else{
+						_oGameViewer.setCurrentGamePiece(new gamepiece.GamePiece(_oGameModel.getCurrentPlayer().getId(), _oGameModel.getCurrentPlayer().getStyle(), _oGameModel.getCurrentPlayer().getWinStyle(), null, nColumnId));
+					}
+				}
+				else{
 					_oGameViewer.setCurrentGamePiece(new gamepiece.GamePiece(_oGameModel.getCurrentPlayer().getId(), _oGameModel.getCurrentPlayer().getStyle(), _oGameModel.getCurrentPlayer().getWinStyle(), null, nColumnId));
 				}
                 
